3cd94462f16d563ee6997b6e20fa1f4b403100df
samuelandrew1 <samuelandrew145@gmail.com>
2026-03-25 11:38:37 -0400

    added car models

--- server/database/app.js
+++ server/database/app.js
@@ -119,3 +119,5 @@ app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+git config --global user.name "samuelandrew"
--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     path('logout', views.logout_request, name="logout"),
     path("register", views.registration, name="register"),
     # path for dealer reviews view
-
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
     # path for add a review view
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
